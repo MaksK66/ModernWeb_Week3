fdb5d3618253aa86fb5f91166e2710fb85098939
MaksK66 <arturkijewski66@gmail.com>
2026-02-02 10:31:19 +0000

    Second part

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ fetch('https://jsonplaceholder.typicode.com/posts/1')
         document.getElementById('output').textContent = data;
     })
     .catch(error => console.error('Error:', error));
+
+fetch('https://dummyjson.com/users/1')
+    .then(response => response.json())
+    .then(data => {
+        const userDiv = document.getElementById('user');
+        userDiv.innerHTML = `
+            <h2>${data.firstName} ${data.lastName}</h2>
+            <p>Email: ${data.email}</p>
+            <p>Phone: ${data.phone}</p>
+            `;
+    });
