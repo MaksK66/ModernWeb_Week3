a614a1ce7c121511140444d77ab62dfc2669311d
MaksK66 <arturkijewski66@gmail.com>
2026-02-02 10:50:26 +0000

    fourth part

---
+++ details.js
@@ -0,0 +1,15 @@
+const params = new URLSearchParams(window.location.search);
+const userID = params.get('id');
+
+fetch('https://dummyjson.com/users/${userID}')
+    .then(response => response.json())
+    .then(data => {
+        const detailsDiv = document.getElementById('staffDetails');
+        detailsDiv.innerHTML = `
+            <h2>${data.firstName} ${data.lastName}</h2>
+            <p>Email: ${data.email}</p>
+            <p>Phone: ${data.phone}</p>
+            <p>Age: ${data.age}</p>
+            <p>Address: ${data.address.city}, ${data.address.state}</p>
+        `;
+    });
