6d2d9046f08bb12259fdf3586f0283a3d072cdc8
MaksK66 <arturkijewski66@gmail.com>
2026-02-02 10:40:16 +0000

    third part

--- script.js
+++ script.js
@@ -16,3 +16,17 @@ fetch('https://dummyjson.com/users/1')
             <p>Phone: ${data.phone}</p>
             `;
     });
+
+fetch('https://dummyjson.com/users')
+    .then(response => response.json())
+    .then(data => {
+        const staffList = document.getElementById('stafflist');
+        data.users.forEach(user => {
+            const staffCard = document.createElement('div');
+            staffCard.innerHTML = `
+                <h2>${user.firstName} ${user.lastName}</h2>
+                <a href="details.html?id=${user.id}">View Details</a>
+            `;
+            staffList.appendChild(staffCard);
+        });
+    });
